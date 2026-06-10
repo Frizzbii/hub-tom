c0e623458cb9df265f30e2c9097b130f2f27656d
aydlig <tom.grandjean@proton.me>
2026-06-10 20:04:30 +0200

    Supabase setup

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,9 +9,9 @@ export default function Navbar() {
         <div className="flex items-center gap-4">
             <Show when="signed-out">
                 <SignInButton>
-                    <button className="bg-gradient-to-r from-emerald-400 to-emerald-600 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                    <button className="bg-gradient-to-r from-emerald-600 to-emerald-400 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                         Sign In
-                </button>
+                    </button>
                 </SignInButton>
                 <SignUpButton>
                     <button className="bg-gradient-to-r from-purple-600 to-purple-400 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
---
+++ src/lib/supabase.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+
+export function createClerkSupabaseClient(getToken: () => Promise<string | null>) {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      accessToken: async () => (await getToken()) ?? ''
+    }
+  )
+}
